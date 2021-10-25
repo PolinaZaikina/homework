6b1a92ea55c26906443e6d238af2f566b231fac3
Polina.Zaikina@urfu.me <Polina.Zaikina@urfu.me>
2021-10-25 14:36:19 +0300

    задание 1

---
+++ Task1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace стихотворение
+
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.ForegroundColor = ConsoleColor.Green;
+			Console.WriteLine("Ночь, улица, фонарь,аптека");
+			{
+				Console.ForegroundColor = ConsoleColor.Yellow;
+				{
+					Console.WriteLine(@"Ночь, улица, фонарь, аптека,
+Бессмысленный и тусклый свет.
+Живи еще хоть четверть века —
+Всё будет так.Исхода нет.
+
+Умрешь — начнешь опять сначала
+И повторится всё, как встарь:
+Ночь, ледяная рябь канала,
+Аптека, улица, фонарь.");
+					Console.ForegroundColor = ConsoleColor.Gray;
+
+					Console.ReadKey();
+				}
+			}
+		}
+	}
+}
+	
+
