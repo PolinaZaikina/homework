e0b4d26aa6306e11b9db3f0565e5d8960eaa3111
Polina.Zaikina@urfu.me <Polina.Zaikina@urfu.me>
2021-10-25 15:44:06 +0300

    задание  3 номер 9

---
+++ Задание 3_9/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_3_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            Console.WriteLine("Введите значение x для вычисления y ");
+            double x = double.Parse(Console.ReadLine());
+            double y = Math.Abs((1 + Math.Sqrt(x + 1)) / (1 - Math.Sqrt(x - 1)));
+            Console.WriteLine("y = "+ y);
+            Console.ReadKey();
+        }
+    }
+}
