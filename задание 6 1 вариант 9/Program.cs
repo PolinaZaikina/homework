cb6c43bbc5a84e7cf2c6557458454dc04a181f78
Polina.Zaikina@urfu.me <Polina.Zaikina@urfu.me>
2021-12-22 23:57:55 +0300

    дз6 1

---
+++ задание 6 1 вариант 9/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace задание_6_1_вариант_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                Console.WriteLine("Введите число,кратное 4 и не оканчивающееся нулем");
+                var n = int.Parse(Console.ReadLine());
+                if (n % 4 != 0)
+                    Console.WriteLine("Число не соответствует условию");
+                else
+                    if (n % 10 != 0)
+                              Console.WriteLine("Переменная не соответствует условию");
+
+                else
+                    Console.WriteLine("Переменная соответствует условию");
+
+                Console.ReadKey();
+            
+        }
+    }
+}
