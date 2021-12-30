22fb3b944b3e5adc1511cb23205d98a2c2605c2e
Polina.Zaikina@urfu.me <Polina.Zaikina@urfu.me>
2021-12-30 22:22:03 +0300

    8.3

---
+++ 8 3 вариант 9/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8_3_вариант_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите последовательность действительных чисел");
+            double a = double.Parse(Console.ReadLine());
+            double result = 1;
+
+            while (a != 0)
+            {
+                result = result*a;
+                a = double.Parse(Console.ReadLine());  
+            }
+            Console.WriteLine($"Результат:{result}") ;
+            Console.ReadKey();
+        }
+    }
+}
