16792f3579e3055796d7915df1da98d51829caa2
Polina.Zaikina@urfu.me <Polina.Zaikina@urfu.me>
2021-12-30 14:17:19 +0300

    13

---
+++ вариант13/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace вариант13
+{
+    class Program
+    {
+        public static string Encryption(string text, int[] key)
+        {
+            string new_text = "";
+            for (int i = 0; i < key.Length; i++)
+            { 
+                new_text += text[key[i] + 3];
+            }
+            return new_text;
+        }
+
+        private static void Main()
+        {
+            Console.WriteLine("Введите текст ");
+            var s = Console.ReadLine().ToUpper();
+            int[] key = { А, Б, В, Г, Д, Е, Ё, Ж, З, И, Й, К, Л, М, Н, О, П, Р, С, Т, У, Ф, Х, Ц, Ч, Ш, Щ, Ъ, Ы, Ь, Э, Ю, Я };
+            var s2 = Encryption(s, key);
+            Console.WriteLine(s2);
+            Console.ReadKey();
+        }
+    }
+}
